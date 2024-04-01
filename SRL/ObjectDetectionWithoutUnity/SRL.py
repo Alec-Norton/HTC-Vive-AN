7d8ca43cff7a0c1031fcedfd83ec365124462733
Alec-Norton <56423293+Alec-Norton@users.noreply.github.com>
2024-04-01 15:27:35 -0400

    Add files via upload

---
+++ SRL/ObjectDetectionWithoutUnity/SRL.py
@@ -0,0 +1,69 @@
+import cv2
+import numpy as np
+print("ok")
+
+net = cv2.dnn.readNet('yolov3.weights', 'yolov3.cfg')
+classes = []
+with open('coco.names', 'r') as f:
+    classes = f.read().splitlines()
+print("ok1")
+cap = cv2.VideoCapture(0)
+#print(classes)
+print("ok2")
+#img = cv2.imread('image.jpg')
+while True:
+    #print("woah")
+    _, img = cap.read()
+    height, width, _ = img.shape
+
+    blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0, 0, 0), swapRB = True, crop = False)
+
+    net.setInput(blob)
+
+    output_layers_names = net.getUnconnectedOutLayersNames()
+    layerOutputs = net.forward(output_layers_names)
+
+    boxes = []
+    confidences = []
+    class_ids = []
+
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0]*width)
+                center_y = int(detection[1]*height)
+                w = int(detection[2]*width)
+                h = int(detection[3]*height)
+
+                x = int(center_x - w/2)
+                y = int(center_y - h/2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    font = cv2.FONT_HERSHEY_PLAIN
+    colors = np.random.uniform(0, 255, size = (len(boxes), 3))
+    if(indexes.count > 0):
+        for i in indexes.flatten():
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            confidence = str(round(confidences[i], 2))
+            color = colors[i]
+            cv2.rectangle(img, (x, y), (x+w, y+h), color, 2)
+    
+    cv2.imshow('Image', img)
+    #print("waiting for key")
+    if cv2.waitKey(1) == ord('q'):
+        #print("are you breaking here?")
+        break
+    
+
+
+cap.release()
+cv2.destroyAllWindows()
